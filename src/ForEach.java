1d2a6971e9d5e5acc1b4516f037357d69e3ea79c
Aniket <deshmukhaniketpradip@gmail.com>
2026-05-24 22:05:47 +0530

    New Commit

---
+++ src/ForEach.java
@@ -0,0 +1,18 @@
+import java.io.*;
+
+class ForEach {
+    public static void main(String[] args)
+    {
+        String names[] = { "Sweta", "Gudly", "Amiya" }; // We can Write like  String[] names 
+
+        for (String name : names) {
+            System.out.println("Name: " + name);
+        }
+        
+		/*
+		 * ForEach a= new ForEach(); System.out.print(a.hashCode());
+		 */
+    }
+    
+    
+}
---
+++ src/TernaryOperator.java
@@ -0,0 +1,8 @@
+class TernaryOperator {
+    public static void main(String args[]) {
+        int a = 10, b = 20;
+        int max = (a > b) ? a : b;
+
+        System.out.println("Maximum is " + max);
+    }
+}
