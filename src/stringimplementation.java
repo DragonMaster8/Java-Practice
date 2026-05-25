03e1d80adaf30b76d9ee5698bb05fa7693b05d7f
Aniket <deshmukhaniketpradip@gmail.com>
2026-05-25 13:09:01 +0530

    Updated

---
+++ src/stringimplementation.java
@@ -0,0 +1,15 @@
+
+public class stringimplementation {
+	public static void main(String[] args) {
+
+		String str = "aniket";
+		String str1 = "desh";
+		int[] a = new int[6];
+		int length = a.length;
+		str.length();
+
+		System.out.print(length + str);
+		
+
+	}
+}
